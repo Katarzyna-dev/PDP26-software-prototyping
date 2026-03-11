0e2ccd01607579431cc73bf2c5c4e6aee50c2454
Katarzyna-dev <katarzyna.rongiers@gmail.com>
2026-03-11 15:46:13 +0200

    lidar file fix

--- lidar-sensor/lidar.py
+++ lidar-sensor/lidar.py
@@ -1,25 +1,26 @@
-## proof of concept
-
 from smbus2 import SMBus, i2c_msg
 import time
 
 address = 0x10
 
-# write Reg_H, Reg_L and Data Length to the sensor, without (!) a STOP
-# request read, specifying the sensor and 7 bytes
+# Data request frame (Reg_H, Reg_L, Data Length)
+write_msg = i2c_msg.write(address, [1, 2, 7])
 
-write = i2c_msg.write(address, [1, 2, 7])
-read = i2c_msg.read(address, 7)
-     
 with SMBus(1) as bus:
-        start = time.time()
-        bus.i2c_rdwr(write, read)
-        data = list(read)
-        TrigFlag = data[0]
-        Dist = ((data[3] << 8 | data[2]))
-        Strength = ((data[5] << 8 | data[4]))
-        Mode = (data[6])
+    while True:
+        try:
+            read_msg = i2c_msg.read(address, 7)
+            bus.i2c_rdwr(write_msg, read_msg)
+            data = list(read_msg)
+            
+            TrigFlag = data[0]
+            Dist = (data[3] << 8) | data[2]
+            Strength = (data[5] << 8) | data[4]
+            Mode = data[6]
+            
+            print(f"Distance: {Dist} cm  Strength: {Strength}  Mode: {Mode}")
         
-        print(Dist)
-        print(Strength)
-        print(Mode)
+        except Exception as e:
+            print("Read error:", e)
+        
+        time.sleep(0.1)  # roughly 10 Hz
